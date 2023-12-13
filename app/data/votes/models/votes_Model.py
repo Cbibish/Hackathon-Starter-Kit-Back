699c66b2e586f858f725e03afbe9a576ca3fc799
Cbibish <sttribet@gmail.com>
2023-12-13 14:02:25 +0100

    debugged version

--- app/data/votes/models/votes_Model.py
+++ app/data/votes/models/votes_Model.py
@@ -5,10 +5,17 @@ from shared import db
 
 class votesModel(db.Model):
     __tablename__ = "votes"
-    id = Column(
+    votes_id = Column(
         Integer,
         primary_key=True,
         unique=True,
         nullable=False
     )
-    message = Column(String(100))
+    opt1=Column(String(255))
+    opt2=Column(String(255))
+    opt3=Column(String(255))
+    opt4=Column(String(255))
+    cnt1=Column(Integer)
+    cnt2=Column(Integer)
+    cnt3=Column(Integer)
+    cnt4=Column(Integer)
